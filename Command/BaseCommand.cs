cef45e6b91563cfadd3024b2bee2c09d90811cb5
raytzou <raytzou@outlook.com>
2024-09-29 00:59:37 +0800

    Add Command plugin

---
+++ Command/BaseCommand.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.Logging;
+
+namespace MyProject
+{
+    public class BaseCommand : MyBasePlugin
+    {
+        public BaseCommand(ILogger<BaseCommand> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        public override string ModuleName => "Base Command";
+        public override string ModuleVersion => "0.87";
+
+        private readonly ILogger<BaseCommand> _logger;
+
+        public override void Load(bool hotReload)
+        {
+            Server.PrintToConsole("base command plugin works");
+        }
+    }
+}
