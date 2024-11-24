71e26d2a2c1fcb6fe2d64abe4576ac98a06da769
raytzou <raytzou@outlook.com>
2024-11-24 15:42:22 +0800

    Revert "delete un-used Interface"
    This reverts commit 6b3196217ca7083986d9dfef3c898e1ceb8d7854.

---
+++ MyProject/Interfaces/IMyBase.cs
@@ -0,0 +1,9 @@
+﻿namespace MyProject.Interface
+{
+    public interface IMyBase
+    {
+        int PlayerCount { get; }
+        int RoundNum { get; }
+        string GetTargetName(string name);
+    }
+}
