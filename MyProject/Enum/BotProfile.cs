4192fd2bce3a9c311291e57bd385bf3e23cd9eb4
raytzou <raytzou@outlook.com>
2025-01-28 03:44:14 +0800

    add BotProfile Enum

---
+++ MyProject/Enum/BotProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace MyProject.Enum
+{
+    public enum BotNameGroup
+    {
+        fumo
+    }
+
+    public enum BotDifficulty
+    {
+        easy,
+        normal,
+        hard,
+        expert,
+        None_10
+    }
+
+    public enum BotGrade
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+}
