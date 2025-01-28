7d0a7c20f940e6c2e9dd6e1b39f6b0d110fa511f
raytzou <raytzou@outlook.com>
2025-01-29 02:36:49 +0800

    add Utility for logger of debug message

---
+++ MyProject/Classes/Utility.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MyProject.Classes
+{
+    public class Utility
+    {
+        public static void DebugLogger<T>(ILogger<T> logger, string content) where T : class
+        {
+            logger.LogInformation(content);
+        }
+    }
+}
