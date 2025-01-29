5869d52e732be6a61c6f625c598888467ae7aafc
raytzou <raytzou@outlook.com>
2025-01-30 04:20:05 +0800

    summary of DebugLogger

--- MyProject/Classes/Utility.cs
+++ MyProject/Classes/Utility.cs
@@ -4,6 +4,9 @@ namespace MyProject.Classes
 {
     public class Utility
     {
+        /// <summary>
+        /// This is for debugging purposes only. There should be no references to this method.
+        /// </summary>
         public static void DebugLogger<T>(ILogger<T> logger, string content) where T : class
         {
             logger.LogInformation(content);
