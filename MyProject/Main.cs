a8378acf5c86b98c7772c0be9465cd30c53c8e39
raytzou <raytzou@outlook.com>
2025-02-16 21:55:16 +0800

    Refactor PlayerDeathHandler for efficiency and safety
    Updated PlayerDeathHandler in Main.cs to use TryGetValue for
    checking and retrieving player positions from the _position
    dictionary. This change improves efficiency by reducing
    redundant dictionary lookups and enhances safety by handling
    missing keys more gracefully. Player position properties
    (Origin, Rotation, Velocity) are now updated directly on the
    retrieved playerPosition object.

--- MyProject/Main.cs
+++ MyProject/Main.cs
@@ -82,15 +82,15 @@ public class Main(
     private HookResult PlayerDeathHandler(EventPlayerDeath @event, GameEventInfo info)
     {
         var player = @event.Userid;
-        if (player is null || !player.IsValid || player.IsBot || !_position.ContainsKey(player.PlayerName)) return HookResult.Continue;
+        if (player is null || !player.IsValid || player.IsBot || !_position.TryGetValue(player.PlayerName, out Position? playerPosition)) return HookResult.Continue;
 
         var origin = new Vector(player.PlayerPawn.Value!.AbsOrigin?.X ?? 0f, player.PlayerPawn.Value!.AbsOrigin?.Y ?? 0f, player.PlayerPawn.Value!.AbsOrigin?.Z ?? 0f);
         var rotation = new QAngle(player.PlayerPawn.Value!.AbsRotation?.X ?? 0f, player.PlayerPawn.Value!.AbsRotation?.Y ?? 0f, player.PlayerPawn.Value!.AbsRotation?.Z ?? 0f);
         var velocity = new Vector(player.PlayerPawn.Value!.AbsVelocity?.X ?? 0f, player.PlayerPawn.Value!.AbsVelocity?.Y ?? 0f, player.PlayerPawn.Value!.AbsVelocity?.Z ?? 0f);
 
-        _position[player.PlayerName].Origin = origin;
-        _position[player.PlayerName].Rotation = rotation;
-        _position[player.PlayerName].Velocity = velocity;
+        playerPosition.Origin = origin;
+        playerPosition.Rotation = rotation;
+        playerPosition.Velocity = velocity;
 
         return HookResult.Continue;
     }
