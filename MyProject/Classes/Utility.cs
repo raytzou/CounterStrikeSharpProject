2f7e2bf8318b9b88259c853a2c79107f6dd461ed
raytzou <raytzou@outlook.com>
2025-03-23 15:08:06 +0800

    Refactor Utility class and add ComputeMeshGroupMask
    Updated using directives in Utility.cs to include MyProject.Models.
    Changed WorkshopSkins and _workshopSkins types to Dictionary<string, SkinInfo>.
    Updated _workshopSkins initialization in static constructor.
    Added ComputeMeshGroupMask method to calculate mesh group mask.

--- MyProject/Classes/Utility.cs
+++ MyProject/Classes/Utility.cs
@@ -2,6 +2,7 @@
 using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API.Modules.Timers;
 using Microsoft.Extensions.Logging;
+using MyProject.Models;
 using System.Reflection;
 using System.Runtime.Serialization;
 
@@ -12,13 +13,13 @@ namespace MyProject.Classes
         public static IEnumerable<string> MapsInPhysicalDirectory => _mapsInPhysicalDirectory;
         public static IEnumerable<string> MapsFromWorkshop => _mapsFromWorkShop;
         public static IEnumerable<string> AllMaps => _mapsInPhysicalDirectory.Concat(_mapsFromWorkShop);
-        public static Dictionary<string, string> WorkshopSkins => _workshopSkins;
+        public static Dictionary<string, SkinInfo> WorkshopSkins => _workshopSkins;
 
         public readonly static List<CounterStrikeSharp.API.Modules.Timers.Timer> _timers;
         private static List<string> _mapsFromWorkShop;
         private static List<string> _mapsInPhysicalDirectory;
         private static readonly Dictionary<CsItem, string> _enumValue;
-        private static readonly Dictionary<string, string> _workshopSkins;
+        private static readonly Dictionary<string, SkinInfo> _workshopSkins;
 
         static Utility()
         {
@@ -76,8 +77,19 @@ namespace MyProject.Classes
                     var split = line.Split(',');
                     var modelName = split[0];
                     var modelPath = split[1];
+                    var armPath = split.Length > 2 ? split[2] : null;
+                    var meshGroupIndex = split.Length > 3 ? split[3] : null;
 
-                    _workshopSkins.Add(modelName, modelPath);
+                    _workshopSkins.Add(modelName, new SkinInfo());
+                    _workshopSkins[modelName].ModelPath = modelPath;
+                    if (!string.IsNullOrEmpty(armPath))
+                        _workshopSkins[modelName].ArmPath = armPath;
+                    else
+                        _workshopSkins[modelName].ArmPath = null;
+                    if (!string.IsNullOrEmpty(meshGroupIndex))
+                        _workshopSkins[modelName].MeshGroupIndex = int.Parse(meshGroupIndex);
+                    else
+                        _workshopSkins[modelName].MeshGroupIndex = null;
                 }
             }
         }
@@ -108,6 +120,36 @@ namespace MyProject.Classes
         public static string GetCsItemEnumValue(CsItem item)
         {
             return _enumValue.TryGetValue(item, out var value) ? value : string.Empty;
+        }
+
+        /// <summary>
+        /// Calculate the mesh group mask for rendering
+        /// </summary>
+        /// <param name="enabledMeshGroups">The specific mesh group you like.</param>
+        /// <param name="fixedMeshGroups">To be honest, idk</param>
+        /// <returns></returns>
+        public static ulong ComputeMeshGroupMask(int[] enabledMeshGroups, Dictionary<int, int> fixedMeshGroups)
+        {
+            ulong meshGroupMask = 0;
+
+            foreach (var meshGroup in enabledMeshGroups)
+            {
+                meshGroupMask |= (ulong)1 << meshGroup;
+            }
+
+            foreach (var fixedMeshGroup in fixedMeshGroups)
+            {
+                if (fixedMeshGroup.Value == 0)
+                {
+                    meshGroupMask &= ~((ulong)1 << fixedMeshGroup.Key);
+                }
+                else
+                {
+                    meshGroupMask |= (ulong)1 << fixedMeshGroup.Key;
+                }
+            }
+
+            return meshGroupMask;
         }
     }
 }
