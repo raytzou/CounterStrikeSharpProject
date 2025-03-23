b4eb0bf3e70833e233ea5455296e42dc5c61dea2
raytzou <raytzou@outlook.com>
2025-03-23 15:07:21 +0800

    add SkinInfo model

---
+++ MyProject/Models/SkinInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MyProject.Models
+{
+    public class SkinInfo
+    {
+        public string ModelPath { get; set; } = null!;
+        public string? ArmPath { get; set; }
+        public int? MeshGroupIndex { get; set; }
+    }
+}
