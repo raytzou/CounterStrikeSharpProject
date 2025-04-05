0e5486204d8d537d403665b8bae7ccfa2c307ec1
raytzou <raytzou@outlook.com>
2025-04-06 02:09:18 +0800

    add Player domain model

---
+++ MyProject/Domains/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProject.Domains
+{
+    [Table("player")]
+    public class Player
+    {
+        [Key]
+        [Column("steam_id")]
+        public ulong SteamId { get; set; }
+        [Column("player_name")]
+        public string PlayerName { get; set; } = null!;
+        [Column("ip_address")]
+        public string IpAddress { get; set; } = null!;
+        [Column("last_time_connect")]
+        public DateTime LastTimeConnect { get; set; }
+    }
+}
