a61f91091b83ec3995bdc50f0a8b4e998d396e5f
raytzou <raytzou@outlook.com>
2025-04-06 02:09:38 +0800

    add ProjectDbContext

---
+++ MyProject/Classes/ProjectDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject.Domains;
+
+namespace MyProject.Classes
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; } = null!;
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Player>()
+                .HasKey(p => p.SteamId);
+        }
+    }
+}
