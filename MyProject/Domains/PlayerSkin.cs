71f575d7847308395a0db6802fd642e81b26eb17
raytzou <raytzou@outlook.com>
2025-04-07 00:59:26 +0800

    add PlayerSkin domain model

---
+++ MyProject/Domains/PlayerSkin.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProject.Domains
+{
+    [Table("player_skin")]
+    public class PlayerSkin
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("")]
+        [Column("steam_id")]
+        public ulong SteamId { get; set; }
+
+        [Column("skin_name")]
+        public string SkinName { get; set; } = null!;
+
+        [Column("acquired_at")]
+        public DateTime AcquiredAt { get; set; }
+
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+
+        [Column("expires_at")]
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
