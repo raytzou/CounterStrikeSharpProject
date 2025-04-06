485f7e63eac20287322ebab471dc73ba4dfa5d60
raytzou <raytzou@outlook.com>
2025-04-07 01:00:07 +0800

    Add IPlayerSkinService interface and PlayerSkinService class
    Introduce IPlayerSkinService with Update, Reset, and GetActiveSkinName
    methods for managing player skins by Steam ID. Implement
    PlayerSkinService in MyProject.Services, using ProjectDbContext to
    interact with the database. Methods include retrieving active skin
    name, deactivating all active skins, and updating or adding new skins.

---
+++ MyProject/Services/Interfaces/IPlayerSkinService.cs
@@ -0,0 +1,11 @@
+﻿namespace MyProject.Services.Interfaces
+{
+    public interface IPlayerSkinService
+    {
+        void Update(ulong steamId, string skinName);
+
+        void Reset(ulong steamId);
+
+        string GetActiveSkinName(ulong steamId);
+    }
+}
---
+++ MyProject/Services/PlayerSkinService.cs
@@ -0,0 +1,65 @@
+﻿using CounterStrikeSharp.API.Core;
+using MyProject.Classes;
+using MyProject.Domains;
+using MyProject.Services.Interfaces;
+
+namespace MyProject.Services
+{
+    public class PlayerSkinService : IPlayerSkinService
+    {
+        private readonly ProjectDbContext _dbContext;
+
+        public PlayerSkinService(ProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public string GetActiveSkinName(ulong steamId) => _dbContext.PlayerSkins
+            .FirstOrDefault(x => x.SteamId == steamId && x.IsActive)?.SkinName ?? string.Empty;
+
+        public void Reset(ulong steamId)
+        {
+            var currentSkins = _dbContext.PlayerSkins
+                .Where(x => x.SteamId == steamId && x.IsActive)
+                .ToList();
+            
+            currentSkins.ForEach(x =>
+            {
+                x.IsActive = false;
+            });
+
+            _dbContext.PlayerSkins.UpdateRange(currentSkins);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(ulong steamId, string skinName)
+        {
+            Reset(steamId);
+
+            var skin = _dbContext.PlayerSkins
+                .FirstOrDefault(x => x.SteamId == steamId && x.SkinName == skinName);
+
+            if (skin is null)
+            {
+                var newSkin = new PlayerSkin
+                {
+                    Id = Guid.NewGuid(),
+                    SteamId = steamId,
+                    SkinName = skinName,
+                    AcquiredAt = DateTime.Now,
+                    IsActive = true,
+                    ExpiresAt = null
+                };
+
+                _dbContext.PlayerSkins.Add(newSkin);
+            }
+            else
+            {
+                skin.IsActive = true;
+                _dbContext.PlayerSkins.Update(skin);
+            }
+
+            _dbContext.SaveChanges();
+        }
+    } 
+}
