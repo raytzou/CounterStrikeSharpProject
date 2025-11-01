a5f5d07a203b99661776a3b58116c03d6ceb1f06
raytzou <raytzou@outlook.com>
2025-11-02 02:57:38 +0800

    chore: add IMusic interface

---
+++ MyProject/Modules/Interfaces/IMusic.cs
@@ -0,0 +1,13 @@
+﻿namespace MyProject.Modules.Interfaces
+{
+    public interface IMusic
+    {
+        void PlayWarmupMusic();
+
+        void PlayRoundMusic();
+
+        void PlayRoundEndMusic();
+
+        void PlayEndGameMusic();
+    }
+}
---
+++ MyProject/Modules/Music.cs
@@ -0,0 +1,27 @@
+﻿using MyProject.Modules.Interfaces;
+
+namespace MyProject.Modules
+{
+    public class Music : IMusic
+    {
+        public void PlayEndGameMusic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayRoundEndMusic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayRoundMusic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayWarmupMusic()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
