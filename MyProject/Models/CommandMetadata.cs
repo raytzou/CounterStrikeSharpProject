b1b81f1aaf95194c7e13b35be6d375ac573b1070
raytzou <raytzou@outlook.com>
2025-12-26 17:55:47 +0800

    feat: add CommandMetadata record for command info

---
+++ MyProject/Models/CommandMetadata.cs
@@ -0,0 +1,12 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace MyProject.Models
+{
+    public record CommandMetadata
+    {
+        public required string Name { get; init; }
+        public required string Description { get; init; }
+        public string[]? Permissions { get; init; }
+    }
+}
